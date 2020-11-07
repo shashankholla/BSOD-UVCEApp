1f257727bac5b16cef56eed1e30fce36857b12d7
Shahsank Holla <shashankholla.teamchimera@gmail.com>
2020-11-07 15:35:37 +0530

    Added jobs

---
+++ App/app/src/main/java/com/example/bsod_uvce/profiles/jobAward.java
@@ -0,0 +1,17 @@
+package com.example.bsod_uvce.profiles;
+
+public class jobAward {
+    String medal;
+    String awardName;
+    String dateAwarded;
+
+    public jobAward(String medal, String awardName, String dateAwarded) {
+        this.medal = medal;
+        this.awardName = awardName;
+        this.dateAwarded = dateAwarded;
+    }
+
+
+
+
+}
