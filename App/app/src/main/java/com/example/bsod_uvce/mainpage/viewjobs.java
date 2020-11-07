6cd30912439002ba8130e757a52c393b316fd66a
Shahsank Holla <shashankholla.teamchimera@gmail.com>
2020-11-07 10:34:16 +0530

    Inital activites structure

---
+++ App/app/src/main/java/com/example/bsod_uvce/mainpage/viewjobs.java
@@ -0,0 +1,16 @@
+package com.example.bsod_uvce.mainpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bsod_uvce.R;
+
+public class viewjobs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewjobs);
+    }
+}
---
+++ App/app/src/main/java/com/example/bsod_uvce/profiles/employerProfile.java
@@ -0,0 +1,16 @@
+package com.example.bsod_uvce.profiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bsod_uvce.R;
+
+public class employerProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer_profile);
+    }
+}
